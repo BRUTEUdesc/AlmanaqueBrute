e064614abbfd9fff2cc2a5a4338b9d8d9ed71e64
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-01-31 16:44:26 -0300

    Update interval_tree.cpp

--- Estrutura de dados/Interval Tree/interval_tree.cpp
+++ Estrutura de dados/Interval Tree/interval_tree.cpp
@@ -9,29 +9,36 @@ using namespace __gnu_pbds;
 struct interval{
     ll lo, hi, id;
     bool operator <(const interval& i) const{
-        return lo < i.lo || (lo == i.lo && hi < i.hi);
+        return lo < i.lo || (lo == i.lo && hi < i.hi) || (lo == i.lo && hi == i.hi && id < i.id);
     }
 };
 template<class CNI, class NI, class Cmp_Fn, class Allocator>
 struct intervals_node_update{
     typedef ll metadata_type;
-    vector<int> vec;
-    void overlaps(const interval& inter, const CNI it){
-        CNI end_it = node_end();
-        if (it == end_it) return;
-        if (inter.hi >= (*it)->lo && inter.lo <= (*it)->hi) vec.push_back((*it)->id);
-        const CNI l_it = it.get_l_child(), r_it = it.get_r_child();
-        const ll l_max = (l_it == end_it)? -INF: l_it.get_metadata();
-        if (l_max >= inter.lo) overlaps(inter, l_it);
-        if ((*it)->lo <= inter.hi) overlaps(inter, r_it);
+    int sz = 0;
+    virtual CNI node_begin() const = 0;
+    virtual CNI node_end() const = 0;
+    
+    inline vector<int> overlaps(const ll l, const ll r){
+        const interval inter = {l, r, 0};
+        queue<CNI> q; q.push(node_begin());
+        vector<int> vec;
+        while(!q.empty()){
+            CNI it = q.front(); q.pop();
+            if (it == node_end()) continue;
+            if (inter.hi >= (*it)->lo && inter.lo <= (*it)->hi) vec.push_back((*it)->id);
+            CNI l_it = it.get_l_child();
+            ll l_max = (l_it == node_end())? -INF: l_it.get_metadata();
+            if (l_max >= inter.lo) q.push(l_it);
+            if ((*it)->lo <= inter.hi) q.push(it.get_r_child());    
+        }
+        return vec;
     }
-    vector<int> overlaps(ll a, ll b){vec.clear(); overlaps({a, b}, node_begin()); return vec;}
+
     inline void operator()(NI it, CNI end_it){
         const ll l_max = (it.get_l_child() == end_it)? -INF: it.get_l_child().get_metadata();
         const ll r_max = (it.get_r_child() == end_it)? -INF: it.get_r_child().get_metadata();
         const_cast<ll&>(it.get_metadata()) = max((*it)->hi, max(l_max, r_max));
     }
-    virtual CNI node_begin() const = 0;
-    virtual CNI node_end() const = 0;
 };
 typedef tree<interval, null_type, less<interval>, rb_tree_tag, intervals_node_update> interval_tree;
