a18d8153bcacda40b124605330ccca1a7b523cf6
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-01-31 22:04:32 -0300

    Update rollback_dsu.cpp

--- Estrutura de dados/Union Find/rollback_dsu.cpp
+++ Estrutura de dados/Union Find/rollback_dsu.cpp
@@ -15,19 +15,13 @@ struct rollback_dsu{
         for(int i = 0; i < n+5; ++i) parent.push_back(i);
     }
     
-    int get(int a){
-        while(a != parent[a]) a = parent[a];
-        return a;
-    }
-    
-    bool same(int a, int b){return get(a) == get(b);}
+    int get(int a){ return (a == parent[a])? a: get(parent[a]);}
+    bool same(int a, int b){ return get(a) == get(b);}
+    void checkpoint(){ changes.push({-2, 0});}
     
     void join(int a, int b){
         a = get(a); b = get(b);
-        if(a == b) {
-            changes.push({-1, -1});
-            return;   
-        }
+        if(a == b){ changes.push({-1, -1}); return;}
         if(size[a] > size[b]) swap(a, b);
         changes.push({a, size[b]});
         parent[a] = b;
@@ -35,11 +29,15 @@ struct rollback_dsu{
         --number_of_sets;
     }
     
-    void rollback(int qnt){
+    void rollback(int qnt = 1<<31){
         for(int i = 0; i < qnt; ++i){
             auto ch = changes.top();
             changes.pop();
             if(ch.node == -1) continue;
+            if(ch.node == -2){
+                if(qnt == 1<<31) break; 
+                --i; continue;
+            }
             size[parent[ch.node]] = ch.old_size;
             parent[ch.node] = ch.node;
             ++number_of_sets;
