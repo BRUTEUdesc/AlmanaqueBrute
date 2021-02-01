286deba8a6b9eea30c33fd5093eff67131f01eb6
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-01 00:19:00 -0300

    Update full_dsu.cpp

--- Estrutura de dados/Union Find/full_dsu.cpp
+++ Estrutura de dados/Union Find/full_dsu.cpp
@@ -2,7 +2,6 @@
 // EXTREMAMENTE PODEROSO
 // Funciona de maneira off-line, vá adicionando as operações e receba as respostas das consultas no retorno da função run
 // O(m*log(m)*log(n)) onde m é o  número de queries e n é o número de nodos
-// ***** POSSIVELMETE INSTAVEL!! *****  (WA em alguns exercícios porém AC em outros)
 
 struct full_dsu{
     struct change{ int node, old_size;};
@@ -19,7 +18,7 @@ struct full_dsu{
         number_of_sets = n;
         loop(i, 0, n+5) parent.push_back(i);
     }
-    
+
     int get(int a){ return (parent[a] == a? a: get(parent[a]));}
     bool same(int a, int b){return get(a) == get(b);}
     void checkpoint(){changes.push({-2, 0});}
@@ -44,7 +43,7 @@ struct full_dsu{
             ++number_of_sets;
         }
     }
-    
+
     void ord(int& a, int& b){if(a > b) swap(a, b);}
     void add(int u, int v){ ord(u, v); edges[{u, v}].push_back({time++, (int)1e9, u, v, 0});}
     void remove(int u, int v){ ord(u, v); edges[{u, v}].back().r = time++;}
@@ -52,25 +51,26 @@ struct full_dsu{
     void question(){ queries.push_back({time, time, 0, 0, 1}); ++time;} // consulta sem nodo
     
     vector<int> solve(){
-        for(auto [p, v]: edges) for(auto q: v) queries.push_back(q);
+        for(auto [p, v]: edges) queries.insert(queries.end(), all(v));
         vector<int> vec(time, -1), ans;
         run(queries, 0, time, vec);
         for(int i: vec) if(i != -1) ans.push_back(i);
         return ans;
     }
     
-    void run(vector<query>& qrs, int l, int r, vector<int>& ans){
+    void run(const vector<query>& qrs, int l, int r, vector<int>& ans){
         if(l > r) return;
-        if(l == r){
-            for(auto q: qrs) if(q.type && q.l == l) ans[l] = same(q.u, q.v); // ou ans[l] = number_of_sets; 
-            return;
-        }
         checkpoint();
         vector<query> qrs_aux;
-        for(auto q: qrs){
-            if (!q.type && q.l <= l && r <= q.r) join(q.u, q.v);
+        for(auto& q: qrs){
+            if (!q.type && q.l <= l && r <= q.r) join(q.u, q.v); 
             else if (r < q.l || l > q.r) continue;
             else qrs_aux.push_back(q);
+        }
+        if(l == r){
+            for(auto& q: qrs) if(q.type && q.l == l) ans[l] = number_of_sets; /*ans[l] = same(q.u, q.v); */
+            rollback();
+            return;
         }
         int m = (l+r)/2;
         run(qrs_aux, l, m, ans);
