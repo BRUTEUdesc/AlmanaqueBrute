ee0db60821a4e4be4b44bc3232125a1e5ff771ca
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-01 01:07:18 -0300

    Update full_dsu.cpp

--- Estrutura de dados/Union Find/full_dsu.cpp
+++ Estrutura de dados/Union Find/full_dsu.cpp
@@ -3,6 +3,7 @@
 // Funciona de maneira off-line, vá adicionando as operações e receba as respostas das consultas no retorno da função run
 // O(m*log(m)*log(n)) onde m é o  número de queries e n é o número de nodos
 // Roda em 0,6 ms para 3e5 queries e nodos com printf e scanf
+// Possivelmente aguenta 1e6 em 3s
 
 struct full_dsu{
     struct change{ int node, old_size;};
