f09506b62a0a752dcbf8f7d9229b82e3f5217377
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-02 18:01:34 -0300

    Create patricia_tree.cpp

---
+++ String/patricia_tree.cpp
@@ -0,0 +1,5 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+
+using namespace __gnu_pbds;
+typedef trie< string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> patrici_tree;
