fe86dad7e3c0caa52bec92657068658f8cbfbd75
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-08 22:03:45 -0300

    Update fftcomp.cpp

--- Matematica/fft/fftcomp.cpp
+++ Matematica/fft/fftcomp.cpp
@@ -1,61 +1,53 @@
+#define loop(x, y, z) for(int x = y; x < z; ++x)
+#define size(x) (int)x.size()
+
+using namespace std;
+ 
 typedef complex<double> cd;
 typedef vector<cd> poly;
+const double PI = acos(-1);
  
-double PI = acos(-1);
- 
-int reverse(int n, int log_n){
-    int res = 0;
-    for(int i = 0; i < log_n; i++)
-        if((1<<i) & n)
-            res |= (1<<(log_n-1-i));
-    return res;
-}
- 
-void fft(poly& a, bool invert){
-    int n = a.size(), log_n = 0;
+void fft(poly& a, bool invert = 0){
+    int n = size(a), log_n = 0;
     while((1 << log_n) < n) log_n++;
     
-    for(int i = 0; i < n; i++){
-        int j = reverse(i, log_n);
-        if(i < j)
-            swap(a[i], a[j]);
+    for(int i = 1, j = 0; i<n; ++i){
+        int bit = n >> 1;
+        for( ; j >= bit; bit >>= 1) j -= bit;
+        j += bit;
+        if (i < j) swap(a[i], a[j]);
     }
+    
     double angle = 2*PI/n * (invert ? -1 : 1);
     poly root(n/2);
-    for(int i = 0; i < n/2; i++)
-        root[i] = cd(cos(angle*i), sin(angle*i));
+    loop(i, 0, n/2) root[i] = cd(cos(angle*i), sin(angle*i));
     
-    for(int len = 2; len <= n; len <<= 1){
-        int step = n/len;
-        int aux = len/2;
-        for(int i = 0; i < n; i+=len)
-            for(int j = 0; j < aux; j++){
+    for(long long len = 2; len <= n; len <<= 1){
+        long long step = n/len;
+        long long aux = len/2;
+        for(long long i = 0; i < n; i+=len)
+            loop(j, 0, aux){
                 cd u = a[i+j], v = a[i+j+aux]*root[step*j];
                 a[i+j] = u + v;
                 a[i+j+aux] = u - v;
             }
     }
-    
-    if(invert){
-        for(int i = 0; i < n; i++)
-            a[i] /= n;
-    }
+    if(invert) loop(i, 0, n) a[i] /= n;
 }
  
-vector<double> solve(vector<int>& a, vector<int>& b){
-    int n = 1, len = a.size() + b.size();
+vector<long long> convolution(vector<long long>& a, vector<long long>& b){
+    int n = 1, len = size(a) + size(b);
     while(n < len) n <<= 1;
     a.resize(n); b.resize(n);
-        
-    poly fft_a(a.begin(), a.end()); fft(fft_a, 0);
-    poly fft_b(b.begin(), b.end()); fft(fft_b, 0);
+    poly fft_a(a.begin(), a.end()); fft(fft_a);
+    poly fft_b(b.begin(), b.end()); fft(fft_b);
     
     poly c(n);
-    for(int i = 0; i < n; i++)
-        c[i] = fft_a[i] / fft_b[i];
-    
+    loop(i, 0, n) c[i] = fft_a[i] * fft_b[i];
     fft(c, 1);
-    vector<double> res(n);
-    while(res.size() > 1 && fabs(res.back()) <= 0.0000002) res.pop_back();
+    
+    vector<long long> res(n);
+    loop(i, 0, n) res[i] = round(c[i].real());
+    // while(size(res) > 1 && res.back() == 0) res.pop_back(); // apenas para quando os zeros direita não importarem
     return res;
 }
