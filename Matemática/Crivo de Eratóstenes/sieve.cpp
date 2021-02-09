efc4885af3d26867283a266c25577e4a24ed6103
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-09 17:56:13 -0300

    Create sieve.cpp

---
+++ Matemática/Crivo de Eratóstenes/sieve.cpp
@@ -0,0 +1,10 @@
+vector<bool> sieve(int n){
+    vector<bool> is_prime(n+5, true);
+    is_prime[0] = false;
+    is_prime[1] = false;
+    long long sq = sqrt(n+5);
+    for(long long i = 2; i <= sq; i++) if(is_prime[i])
+        for (long long j =  i * i; j < LIM; j+= i)
+            is_prime[j] = false;
+    return is_prime;
+}
