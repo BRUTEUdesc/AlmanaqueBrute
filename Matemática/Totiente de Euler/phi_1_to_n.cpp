bc96e5750406cc4308ad36546275a7d9d5ee7c04
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-10 18:53:06 -0300

    Update phi_1_to_n.cpp

--- Matemática/Totiente de Euler/phi_1_to_n.cpp
+++ Matemática/Totiente de Euler/phi_1_to_n.cpp
@@ -1,11 +1,9 @@
 vector<int> phi_1_to_n(int n) {
     vector<int> phi(n + 1);
     for (int i = 0; i <= n; i++) phi[i] = i;
-    for (int i = 2; i <= n; i++) {
-        if (phi[i] == i) {
-            for (int j = i; j <= n; j += i)
-                phi[j] -= phi[j] / i;
-        }
+    for (int i = 2; i <= n; i++) if (phi[i] == i) {
+        for (int j = i; j <= n; j += i)
+            phi[j] -= phi[j] / i;
     }
     return phi;
 }
