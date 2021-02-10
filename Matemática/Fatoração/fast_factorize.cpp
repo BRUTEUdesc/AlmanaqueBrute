57e93f728ca79ff13e39f8978b11be7f15360983
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-09 23:13:52 -0300

    Update fast_factorize.cpp

--- Matemática/Fatoração/fast_factorize.cpp
+++ Matemática/Fatoração/fast_factorize.cpp
@@ -2,10 +2,10 @@
 // usa pollar_rho.cpp!! olhar em matemática/fatoração
 
 vector<long long> factorize(long long n) {
-	if (n == 1) return {};
-	if (miller_rabin(n)) return {n};
-	long long x = pollard_rho(n);
-	auto l = factor(x), r = factor(n / x);
-	l.insert(l.end(), all(r));
-	return l;
+    if (n == 1) return {};
+    if (miller_rabin(n)) return {n};
+    long long x = pollard_rho(n);
+    auto l = factorize(x), r = factorize(n / x);
+    l.insert(l.end(), all(r));
+    return l;
 }
