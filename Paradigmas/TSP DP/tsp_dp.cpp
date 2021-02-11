24e2dd481d5c95c800fed799b802f8304889188b
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-10 22:51:05 -0300

    Update and rename tspdp.cpp to tsp_dp.cpp

--- Paradigmas/TSP DP/tsp_dp.cpp
+++ Paradigmas/TSP DP/tsp_dp.cpp
@@ -5,24 +5,15 @@ long double dp[lim][1<<lim];
 int limMask = (1<<lim) - 1; // 2**(maximo de itens) - 1
 long double solve(int atual, int mask, int n){
     if(dp[atual][mask] != 0) return dp[atual][mask];
-    
     if(mask == (1<<n) -1) return dp[atual][mask] = 0; // o que fazer quando chega no final 
+
     long double res = 1e13;// pode ser maior se precisar
-    if(!mask){
-        for(int i = 0; i < n; i ++){
-            if(!(mask & (1<<i))) {
-                long double aux =  solve(i, mask|(1<<i) , n);
-                res = min(res, aux);
-            }
-        }
-    }else{
-        for(int i = 0; i < n; i ++){
-            if(!(mask & (1<<i))) {
-                long double aux = dist[atual][i] + solve(i, mask|(1<<i) , n);
-                res = min(res, aux);
-            }
+    for(int i = 0; i < n; i ++){
+        if(!(mask & (1<<i))) {
+            long double aux = solve(i, mask|(1<<i) , n);
+            if(mask) aux += dist[atual][i];
+            res = min(res, aux);
         }
     }
-    
     return dp[atual][mask] = res;
 }
