f9845d815cc0a775fb7bee246c0ff4e62891171c
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-18 14:58:42 -0300

    Update and rename expMod.cpp to exp_mod.cpp

--- Matemática/Exponenciação Modular Rápida/exp_mod.cpp
+++ Matemática/Exponenciação Modular Rápida/exp_mod.cpp
@@ -1,4 +1,4 @@
-long long expMod(long long base, long long exp, long long mod){
+long long exp_mod(long long base, long long exp, long long mod){
   long long b = base, res = 1;
   while(exp){
     if(exp & 1) res = (res * b) % mod;
