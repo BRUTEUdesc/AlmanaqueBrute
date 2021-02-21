a81d10d63a2a90c625ec46d0c677edbb6f756aef
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2021-02-21 16:32:08 -0300

    Update ntt.cpp

--- Matemática/NTT/ntt.cpp
+++ Matemática/NTT/ntt.cpp
@@ -1,24 +1,24 @@
 typedef long long ll;
 typedef vector<ll> poly;
 
-ll mod = 998244353LL;
-ll root = 15311432LL;
-ll root_1 = 469870224LL;
-ll root_pw = 1 << 23;
+ll mod[2] = {1004535809LL, 1092616193LL};
+ll root[2] = {12289LL, 23757LL};
+ll root_1[2] = {313564925LL, 642907570LL};
+ll root_pw[2] = {1LL << 21, 1LL << 21};
 
-ll modInv(ll b){
-    ll e = mod - 2;
+ll modInv(ll b, ll m){
+    ll e = m - 2;
     ll res = 1;
     while(e){
-        if(e & 1) res = (res * b) % mod;
+        if(e & 1) res = (res * b) % m;
         e /= 2;
-        b = (b * b) % mod;
+        b = (b * b) % m;
     }
     return res;
 }
 
-void ntt(poly &a, bool invert = 0){
-    ll n = (ll)a.size();
+void ntt(poly &a, bool invert, int id){
+    ll n = (ll)a.size(), m = mod[id];
     for(ll i = 1, j = 0; i<n; ++i){
         ll bit = n >> 1;
         for( ; j >= bit; bit >>= 1) j -= bit;
@@ -26,33 +26,33 @@ void ntt(poly &a, bool invert = 0){
         if (i < j) swap (a[i], a[j]);
     }
     for(ll len = 2, wlen; len <= n; len <<= 1){
-        wlen = invert? root_1 : root;
-        for(ll i = len; i < root_pw; i<<=1)
-            wlen = (wlen * wlen) % mod;
+        wlen = invert? root_1[id] : root[id];
+        for(ll i = len; i < root_pw[id]; i<<=1)
+            wlen = (wlen * wlen) % m;
         for(ll i = 0; i < n; i+=len){
             ll w = 1;
             for(ll j = 0; j < len/2; j++){
-                ll u = a[i+j], v = (a[i+j+len/2] * w) % mod;
-                a[i+j] = (u+v) % mod;
-                a[i+j+len/2] = (u-v+mod) % mod;
-                w = (w * wlen) % mod;
+                ll u = a[i+j], v = (a[i+j+len/2] * w) % m;
+                a[i+j] = (u+v) % m;
+                a[i+j+len/2] = (u-v+m) % m;
+                w = (w * wlen) % m;
              }
         }
     }
     if(invert){
-        ll inv = modInv(n);
+        ll inv = modInv(n, m);
         for(ll i = 0; i < n; i++)
-            a[i] = (a[i] * inv) % mod;
+            a[i] = (a[i] * inv) % m;
     }
 }
 
-poly convolution(poly &a, poly &b){
+poly convolution(poly &a, poly &b, int id = 0){
     ll n = 1LL, len = (1LL + a.size() + b.size());
     while(n < len) n *= 2;
     a.resize(n); b.resize(n);
-    ntt(a); ntt(b);
+    ntt(a, 0, id); ntt(b, 0, id);
     poly answer(n);
     for(ll i = 0; i < n; i++) answer[i] = (a[i] * b[i]);
-    ntt(answer, 1);
+    ntt(answer, 1, id);
     return answer;
 }
