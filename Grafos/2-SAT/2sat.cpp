152afaf68915898a913af93fa668416d0319ae13
Rafael Granza <rafaelgranzademello@gmail.com>
2021-05-17 14:47:47 -0300

    Create 2sat.cpp

---
+++ Grafos/2-SAT/2sat.cpp
@@ -0,0 +1,50 @@
+struct sat2{
+    int n;
+    vector<vector<int>> g, gt;
+    vector<bool> used;
+    vector<int> order, comp;
+    vector<bool> assignment;
+    
+    sat2(int n) : n(n){
+        g.assign(2*n+5, vector<int>());
+        gt.assign(2*n+5, vector<int>());
+    }
+    
+    void add_edge(int v, int u, bool v_is_positive, bool u_is_positive){
+        g[2*v + v_is_positive].push_back(2*u + u_is_positive);
+        gt[2*u + u_is_positive].push_back(2*v + v_is_positive);
+    }
+    
+    void dfs1(int v) {
+        used[v] = true;
+        for (int u : g[v]) if (!used[u])
+            dfs1(u);
+        order.push_back(v);
+    }
+
+    void dfs2(int v, int cl) {
+        comp[v] = cl;
+        for (int u : gt[v]) if (comp[u] == -1)
+            dfs2(u, cl);
+    }
+
+    bool solve_2SAT() {
+        order.clear();
+        used.assign(n, false);
+        for (int i = 0; i < n; ++i) if (!used[i])
+            dfs1(i);
+
+        comp.assign(n, -1);
+        for (int i = 0, j = 0; i < n; ++i) {
+            int v = order[n - i - 1];
+            if (comp[v] == -1) dfs2(v, j++);
+        }
+
+        assignment.assign(n / 2, false);
+        for (int i = 0; i < n; i += 2) {
+            if (comp[i] == comp[i + 1]) return false;
+            assignment[i / 2] = comp[i] > comp[i + 1];
+        }
+        return true;
+    }
+};
