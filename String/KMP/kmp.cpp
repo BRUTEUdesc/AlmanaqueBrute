38868a4f0bb7e87479638c497b1ea5ac9198bf3c
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-05-16 21:00:04 -0300

    Create kmp.cpp

---
+++ String/KMP/kmp.cpp
@@ -0,0 +1,19 @@
+vector <int> pi(string &s) {
+    vector <int> p(s.size());
+    for (int i = 1, j = 0; i < s.size(); i++) {
+        while (j > 0 && s[i] != s[j]) j = p[j-1];
+        if (s[i] == s[j]) j++;
+        p[i] = j;
+    }
+    return p;
+}
+ 
+vector <int> kmp(string &s, string &t) {
+    vector <int> p = pi(t+'$'), match;
+    for (int i = 0, j = 0; i < s.size(); i++) {
+        while (j > 0 && s[i] != t[j]) j = p[j-1];
+        if (s[i] == t[j]) j++;
+        if (j == t.size()) match.push_back(i-j+1); 
+    }
+    return match;
+}
