603c806e42754837ffab5a03d9ba3f1d25eac764
Rafael Granza <rafaelgranzademello@gmail.com>
2021-05-17 21:49:34 -0300

    Update op_queue.cpp

--- Estruturas de Dados/Operation Queue/op_queue.cpp
+++ Estruturas de Dados/Operation Queue/op_queue.cpp
@@ -2,26 +2,22 @@ template <typename T>
 struct op_queue{
     stack<pair<T, T>> s1, s2;
     T result;
-    
     T op(T a, T b){
         return a; // TODO: op to compare
         // min(a, b);
         // gcd(a, b);
         // lca(a, b);
     }
-    
     T get(){
         if (s1.empty() || s2.empty()) 
             return result = s1.empty() ? s2.top().second : s1.top().second;
         else
             return result = op(s1.top().second, s2.top().second);
     }
-    
     void add(T element){
         result = s1.empty() ? element : op(element, s1.top().second);
         s1.push({element, result});
     }
-    
     void remove(){
         if (s2.empty()) {
             while (!s1.empty()) {
