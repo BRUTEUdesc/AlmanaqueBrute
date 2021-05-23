f00f9c54460e3ecf3c94e754bbca96acb0b07789
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-05-22 21:45:11 -0300

    Create extended_GCD.cpp

---
+++ Matemática/GCD/extended_GCD.cpp
@@ -0,0 +1,11 @@
+int extended_gcd(int a, int b, int& x, int& y) {
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1;
+    while (b) {
+        int q = a / b;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a, b) = make_tuple(b, a - q * b);
+    }
+    return a;
+}
