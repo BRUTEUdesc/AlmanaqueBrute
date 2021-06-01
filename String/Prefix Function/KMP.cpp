0b554ed1ca7e67d5b3d3e991d04a4ca930b0bce0
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-01 11:40:28 -0300

    Update KMP.cpp

--- String/Prefix Function/KMP.cpp
+++ String/Prefix Function/KMP.cpp
@@ -8,12 +8,13 @@ vector <int> pi(string &s) {
     return p;
 }
  
-vector <int> kmp(string &s, string &t) {
-    vector <int> p = pi(t+'$'), match;
+vector <int> kmp(string &s, string t) {
+    t += '$';
+    vector <int> p = pi(t), match;
     for (int i = 0, j = 0; i < s.size(); i++) {
         while (j > 0 && s[i] != t[j]) j = p[j-1];
         if (s[i] == t[j]) j++;
-        if (j == t.size()) match.push_back(i-j+1); 
+        if (j == t.size() - 1) match.push_back(i-j+1); 
     }
     return match;
 }
