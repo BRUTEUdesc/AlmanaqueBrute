5d5626dd00fb0ad69a22fa0e42f792b1b688f8e6
Rafael Granza <rafaelgranzademello@gmail.com>
2021-06-03 14:14:31 -0300

    Create trie.cpp

---
+++ String/Trie/trie.cpp
@@ -0,0 +1,24 @@
+struct trie{
+ 
+    map<char, int> trie[100005];
+    int value[100005];
+    int n_nodes = 0;
+    
+    void insert(string s, int value){
+        int id = 0;
+        for (char c: s){
+          if(!trie[id].count(c)) trie[id][c] = ++n_nodes;
+          id = trie[id][c];
+        }
+        value[id] = value;
+    }
+ 
+    int get_value(string s){
+        int id = 0;
+        for (char c: s){
+          if(!trie[id].count(c)) return -1;
+          id = trie[id][c];
+        }
+        return value[id];
+    }
+};
