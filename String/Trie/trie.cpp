a7ffb46e98361a4e68c9365cc4aa74a80c642f2d
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-03 17:44:13 -0300

    Update trie.cpp

--- String/Trie/trie.cpp
+++ String/Trie/trie.cpp
@@ -2,13 +2,13 @@ struct trie{
     map<char, int> trie[100005];
     int value[100005];
     int n_nodes = 0;
-    void insert(string &s, int value){
+    void insert(string &s, int v){
         int id = 0;
         for (char c: s){
           if(!trie[id].count(c)) trie[id][c] = ++n_nodes;
           id = trie[id][c];
         }
-        value[id] = value;
+        value[id] = v;
     }
     int get_value(string &s){
         int id = 0;
