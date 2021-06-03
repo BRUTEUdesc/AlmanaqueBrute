3b27aec853b660f78a081a92e719bebdc6bfb03e
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-03 09:04:12 -0300

    tab 4

--- Matemática/Exponenciação Modular Rápida/exp_mod.cpp
+++ Matemática/Exponenciação Modular Rápida/exp_mod.cpp
@@ -1,9 +1,9 @@
 long long exp_mod(long long base, long long exp, long long mod){
-  long long b = base, res = 1;
-  while(exp){
-    if(exp & 1) res = (res * b) % mod;
-    b = (b * b) % mod;
-    exp /= 2;
-   }
-   return res;
+    long long b = base, res = 1;
+    while(exp){
+        if(exp & 1) res = (res * b) % mod;
+        b = (b * b) % mod;
+        exp /= 2;
+    }
+    return res;
 }
