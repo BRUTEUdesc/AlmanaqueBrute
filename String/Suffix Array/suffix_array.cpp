b871105d88f8134267fcb7ad21c0780055cfe15f
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-17 17:57:44 -0300

    Update suffix_array.cpp

--- String/Suffix Array/suffix_array.cpp
+++ String/Suffix Array/suffix_array.cpp
@@ -1,58 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+const int MAX_N = 1e5+5;
 
-#define MAX 100005 // suports O(n log n)
-
-string s; // the input string, up to MAX characters
-int n; // the length of input string
-
-int ra[MAX], auxra[MAX]; // rank array and temporary rank array
-int sa[MAX], auxsa[MAX]; // suffix array and temporary suffix array
-
-int c[MAX]; // for counting/radix sort
-void countingSort(int k) { // O(n)
-    memset(c, 0, sizeof c); // clear frequency table
-    for (int i = 0; i < n; i++) c[(i + k < n)? ra[i + k] : 0]++; // count the frequency of each integer rank
-
-    int sum = 0, maxi = max(255, n); // up to 255 ASCII chars or length of n
-    for (int i = 0; i < maxi; i++) {
-        int t = c[i];
-        c[i] = sum;
-        sum += t;
-    }
-
-    for (int i = 0; i < n; i++) auxsa[c[sa[i]+k < n ? ra[sa[i]+k] : 0]++] = sa[i]; // shuffle the suffix array if necessary
-    for (int i = 0; i < n; i++) sa[i] = auxsa[i]; // update the suffix array sa
-}
-
-void constructSA() {
-    for (int i = 0; i < n; i++) {
-        ra[i] = s[i]; // initial rankings
-        sa[i] = i; // initial sa: {0, 1, 2, ..., n-1}
-    }
-
-    for (int k = 1; k < n; k <<= 1) { // repeat sorting process log n times
-        countingSort(k); // actually radix sort: sort based on the k-th item
-        countingSort(0); // then (stable) sort based on the first item
-
-        int r = 0;
-        auxra[sa[0]] = r; // re-ranking; start from rank r = 0
-        for (int i = 1; i < n; i++) { // compare adjacent suffixes
-            // if same pair => same rank; otherwise, increase rank
-            if (ra[sa[i-1]] != ra[sa[i]] || ra[sa[i-1]+k] != ra[sa[i]+k]) r++;
-            auxra[sa[i]] = r;
+struct suffix_array {
+    int n, ra[MAX_N], sa[MAX_N], auxra[MAX_N], auxsa[MAX_N], c[MAX_N];
+    void counting_sort(int k) {
+        memset(c, 0, sizeof(c));
+        for (int i = 0; i < n; i++) c[(i + k < n)? ra[i + k] : 0]++;
+        int sum = 0, maxi = max(256, n);
+        for (int i = 0; i < maxi; i++) {
+            int t = c[i];
+            c[i] = sum;
+            sum += t;
         }
-
-        for (int i = 0; i < n; i++) ra[i] = auxra[i]; // update the rank array ra
-        if (ra[sa[n-1]] == n-1) break; // nice optimization trick
+        for (int i = 0; i < n; i++) auxsa[c[sa[i]+k < n? ra[sa[i]+k]:0]++] = sa[i];
+        for (int i = 0; i < n; i++) sa[i] = auxsa[i];
     }
-}
-
-int main() {
-    cin >> s; // input str as normal, without the ‘$’
-    s += '$'; // add terminating character
-    n = s.size(); // set str size
-
-    constructSA();
-    for (int i = 0; i < n; i++) printf("%d: %s\n", sa[i], s.c_str() + sa[i]);
-}
+    void build_sa() {
+        for (int k = 1; k < n; k <<= 1) {
+            counting_sort(k);
+            counting_sort(0);
+            int r = 0;
+            auxra[sa[0]] = 0;
+            for (int i = 1; i < n; i++) auxra[sa[i]] = (ra[sa[i]] == ra[sa[i-1]] && ra[sa[i]+k] == ra[sa[i-1]+k])? r:++r;
+            for (int i = 0; i < n; i++) ra[i] = auxra[i];
+            if (ra[sa[n-1]] == n-1) break;
+        }
+    }
+    suffix_array(string s) {
+        s += '$';
+        n = s.size();
+        for (int i = 0; i < n; i++) {
+            ra[i] = s[i];
+            sa[i] = i;
+        }
+        build_sa();
+        // for (int i = 0; i < n; i++) printf("%2d %2d: %s\n", i, sa[i], s.c_str() + sa[i]);
+    }
+};
