8db566a43412b8b0baa99c396e995efcd987a1df
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-17 18:05:40 -0300

    Update suffix_array.cpp

--- String/Suffix Array/suffix_array.cpp
+++ String/Suffix Array/suffix_array.cpp
@@ -1,16 +1,11 @@
 const int MAX_N = 1e5+5;
 
 struct suffix_array {
-    int n, ra[MAX_N], sa[MAX_N], auxra[MAX_N], auxsa[MAX_N], c[MAX_N];
+    int n, sum, r, ra[MAX_N], sa[MAX_N], auxra[MAX_N], auxsa[MAX_N], c[MAX_N];
     void counting_sort(int k) {
         memset(c, 0, sizeof(c));
         for (int i = 0; i < n; i++) c[(i + k < n)? ra[i + k] : 0]++;
-        int sum = 0, maxi = max(256, n);
-        for (int i = 0; i < maxi; i++) {
-            int t = c[i];
-            c[i] = sum;
-            sum += t;
-        }
+        for (int i = sum = 0; i < max(256, n); i++) sum += c[i], c[i] = sum - c[i];
         for (int i = 0; i < n; i++) auxsa[c[sa[i]+k < n? ra[sa[i]+k]:0]++] = sa[i];
         for (int i = 0; i < n; i++) sa[i] = auxsa[i];
     }
@@ -18,8 +13,7 @@ struct suffix_array {
         for (int k = 1; k < n; k <<= 1) {
             counting_sort(k);
             counting_sort(0);
-            int r = 0;
-            auxra[sa[0]] = 0;
+            auxra[sa[0]] = r = 0;
             for (int i = 1; i < n; i++) auxra[sa[i]] = (ra[sa[i]] == ra[sa[i-1]] && ra[sa[i]+k] == ra[sa[i-1]+k])? r:++r;
             for (int i = 0; i < n; i++) ra[i] = auxra[i];
             if (ra[sa[n-1]] == n-1) break;
@@ -28,10 +22,7 @@ struct suffix_array {
     suffix_array(string s) {
         s += '$';
         n = s.size();
-        for (int i = 0; i < n; i++) {
-            ra[i] = s[i];
-            sa[i] = i;
-        }
+        for (int i = 0; i < n; i++) ra[i] = s[i], sa[i] = i;
         build_sa();
         // for (int i = 0; i < n; i++) printf("%2d %2d: %s\n", i, sa[i], s.c_str() + sa[i]);
     }
