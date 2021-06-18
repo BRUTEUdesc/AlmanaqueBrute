577529866b58d7b991b10af903805b5d874ce99c
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2021-06-17 21:47:33 -0300

    Update busca.cpp

--- String/Suffix Array/busca.cpp
+++ String/Suffix Array/busca.cpp
@@ -1,16 +1,16 @@
-pair<int, int> busca(char c, pair<int, int> &range, int delta) {
+pair<int, int> busca(string &s, int idx, pair<int, int> &range) {
     int esq = range.first, dir = range.second, L = -1, R = -1;
     while (esq <= dir) {
         int mid = (esq + dir)/2;
-        if (s[sa[mid]+delta] == c) L = mid;
-        if (s[sa[mid]+delta] < c) esq = mid+1;
+        if (s[sa[mid]+idx] == s[idx]) L = mid;
+        if (s[sa[mid]+idx] < s[idx]) esq = mid+1;
         else dir = mid-1; 
     }
     esq = range.first, dir = range.second;
     while (esq <= dir) {
         int mid = (esq + dir)/2;
-        if (s[sa[mid]+delta] == c) R = mid;
-        if (s[sa[mid]+delta] <= c) esq = mid+1;
+        if (s[sa[mid]+idx] == s[idx]) R = mid;
+        if (s[sa[mid]+idx] <= s[idx]) esq = mid+1;
         else dir = mid-1; 
     }
     return {L, R};
@@ -19,7 +19,7 @@ pair<int, int> busca(char c, pair<int, int> &range, int delta) {
 bool busca_string(string &s) {
     ii range = {0, sa.n-1};
     for (int i = 0; i < s.size(); i++) {
-        range = busca(s[i], range, i);
+        range = busca(s, i, range);
         if (range.first == -1) return false;
     }
     return true;
