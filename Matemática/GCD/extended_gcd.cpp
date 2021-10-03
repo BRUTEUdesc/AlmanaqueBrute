de3e755c4476ca49663593f6d6afa31b29618610
Rafael Granza <rafaelgranzademello@gmail.com>
2021-10-03 00:15:32 -0300

    Update extended_gcd.cpp

--- Matemática/GCD/extended_gcd.cpp
+++ Matemática/GCD/extended_gcd.cpp
@@ -1,11 +1,9 @@
-int extended_gcd(int a, int b, int& x, int& y) {
-    x = 1, y = 0;
-    int x1 = 0, y1 = 1;
-    while (b) {
-        int q = a / b;
-        tie(x, x1) = make_tuple(x1, x - q * x1);
-        tie(y, y1) = make_tuple(y1, y - q * y1);
-        tie(a, b) = make_tuple(b, a - q * b);
+ll extended_gcd(ll a , ll b , ll& x , ll& y ) {
+    if(b == 0){
+        x = 1; y = 0; return a;
+    }else{
+        ll g = extended_gcd(b, a % b, y, x);
+        y -= a / b * x;
+        return g;
     }
-    return a;
 }
