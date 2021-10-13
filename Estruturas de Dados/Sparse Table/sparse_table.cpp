b2f545d700d251b2003607408117bd3724b9a668
Rafael Granza <rafaelgranzademello@gmail.com>
2021-10-13 16:04:49 -0300

    Update sparse_table.cpp

--- Estruturas de Dados/Sparse Table/sparse_table.cpp
+++ Estruturas de Dados/Sparse Table/sparse_table.cpp
@@ -6,7 +6,8 @@ struct SparseTable {
         for (int i = 0; i < n; i++) st[0][i] = v[i];
         for (int i = 1; i <= e; i++) {
             for (int j = 0; j + (1 << i) <= n; j++) {
-                st[i][j] = min(st[i-1][j], st[i-1][j+(1 << (i-1))]);
+                st[i][j] = min(st[i-1][j], st[i-1][j+(1 << (i-1))]); // query de range
+                // st[i][j] = min(st[i-1][j], st[i-1][st[i-1][j]]); // query de caminho
             }
         }
     }
