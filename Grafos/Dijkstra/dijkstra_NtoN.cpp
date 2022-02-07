020df996fc3ab6fe76f29fcfb6c34b5984929d1c
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2022-02-06 21:17:25 -0300

    Create dijkstra_NtoN.cpp

---
+++ Grafos/Dijkstra/dijkstra_NtoN.cpp
@@ -0,0 +1,26 @@
+const int MAX = 505, INF = 1e9+9;
+
+vector <ii> adj[MAX];
+int dist[MAX][MAX];
+
+void dk(int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            dist[i][j] = INF;
+    for (int s = 0; s < n; s++) {
+        priority_queue <ii, vector<ii>, greater<ii>> fila;
+        dist[s][s] = 0;
+        fila.emplace(dist[s][s], s);
+        while (!fila.empty()) {
+            auto [d, u] = fila.top();
+            fila.pop();
+            if (d != dist[s][u]) continue;
+            for (auto [w, v] : adj[u]) {
+                if (dist[s][v] > d + w) {
+                    dist[s][v] = d + w;
+                    fila.emplace(dist[s][v], v);
+                }
+            }
+        }
+    }
+}
