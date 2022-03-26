df38d86c5a0baffc494ad3bf3c86f5c9f519b179
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2022-03-26 15:53:08 -0300

    Create find_bridges.cpp

---
+++ Grafos/bridge/find_bridges.cpp
@@ -0,0 +1,35 @@
+int n; // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int u, int p = -1) {
+    visited[u] = true;
+    tin[u] = low[u] = timer++;
+    for (int v : adj[u]) {
+        if (v == p) continue;
+        if (visited[v]) {
+            low[u] = min(low[u], tin[v]);
+        } else {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] > tin[u]) {
+                // edge UV is a bridge
+                // do_something(u, v)
+            }
+        }
+    }
+}
+
+void find_bridges() {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs(i);
+    }
+}
