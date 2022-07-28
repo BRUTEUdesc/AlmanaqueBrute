08018f15149b43840a9b82bf5df9f03154240d3d
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2022-07-28 15:37:22 -0300

    Update seg_tree.cpp

--- Estruturas de Dados/Segment Tree/seg_tree.cpp
+++ Estruturas de Dados/Segment Tree/seg_tree.cpp
@@ -1,47 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-typedef vector <int> vi;
+namespace seg {
+    const int MAX = 1e5+5;
 
-const int MAX = 1e5+5;
+    int n, tree[4*MAX];
 
-int n, tree[4*MAX];
+    int le(int n) { return 2*n+1; }
+    int ri(int n) { return 2*n+2; }
 
-int le(int n) {return 2*n+1;}
-int ri(int n) {return 2*n+2;}
-
-void build(int n, int esq, int dir, vi &v) {
-    if (esq == dir) tree[n] = v[esq];
-    else {
-        int mid = (esq + dir) / 2;
-        build(le(n), esq, mid, v);
-        build(ri(n), mid+1, dir, v);
-        tree[n] = tree[le(n)] + tree[ri(n)];
+    void build(int n, int esq, int dir, vector<long long> &v) {
+        if (esq == dir) tree[n] = v[esq];
+        else {
+            int mid = (esq + dir) / 2;
+            build(le(n), esq, mid, v);
+            build(ri(n), mid+1, dir, v);
+            tree[n] = tree[le(n)] + tree[ri(n)];
+        }
     }
-}
-void build(vi &v) {build(0, 0, n-1, v);}
+    void build(vector<long long> &v) { build(0, 0, n-1, v); }
 
-int query(int n, int esq, int dir, int l, int r) {
-    if (esq > r || dir < l) return 0;
-    if (l <= esq && dir <= r) return tree[n];
-    int mid = (esq + dir) / 2;
-    return query(le(n), esq, mid, l, r)
-           + query(ri(n), mid+1, dir, l, r);
-}
-int query(int l, int r) {return query(0, 0, n-1, l, r);}
-
-void update(int n, int esq, int dir, int x, int v) {
-    if (esq > x || dir < x) return;
-    if (esq == dir) tree[n] = v;
-    else {
+    int query(int n, int esq, int dir, int l, int r) {
+        if (esq > r || dir < l) return 0;
+        if (l <= esq && dir <= r) return tree[n];
         int mid = (esq + dir) / 2;
-        if (x <= mid) update(le(n), esq, mid, x, v);
-        else update(ri(n), mid+1, dir, x, v);
-        tree[n] = tree[le(n)] + tree[ri(n)];
+        return query(le(n), esq, mid, l, r)
+            + query(ri(n), mid+1, dir, l, r);
     }
-}
-void update(int x, int v) {update(0, 0, n-1, x, v);}
+    int query(int l, int r) { return query(0, 0, n-1, l, r); }
 
-int main() {
-    cin >> n;
+    void update(int n, int esq, int dir, int x, int v) {
+        if (esq > x || dir < x) return;
+        if (esq == dir) tree[n] = v;
+        else {
+            int mid = (esq + dir) / 2;
+            if (x <= mid) update(le(n), esq, mid, x, v);
+            else update(ri(n), mid+1, dir, x, v);
+            tree[n] = tree[le(n)] + tree[ri(n)];
+        }
+    }
+    void update(int x, int v) { update(0, 0, n-1, x, v); }
 }
