0ac058a3e826d51804b6ed1587af6a9513992bc9
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2023-02-06 09:47:07 -0300

    Update seg_tree_lazy.cpp

--- Estruturas de Dados/Segment Tree/seg_tree_lazy.cpp
+++ Estruturas de Dados/Segment Tree/seg_tree_lazy.cpp
@@ -1,60 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef vector <int> vi;
-
-const int MAX = 1e5+5;
-
-int n, tree[4*MAX], lazy[4*MAX];
-
-int le(int n) {return 2*n+1;}
-int ri(int n) {return 2*n+2;}
-
-void push(int n, int esq, int dir) {
-    tree[n] += lazy[n] * (dir - esq + 1);
-    if (esq != dir) {
-        lazy[le(n)] += lazy[n];
-        lazy[ri(n)] += lazy[n];
+namespace seg {
+    const ll MAX = 2e5+5;
+    const ll NEUTRAL = 0; // merge(a, neutral) = a
+    ll merge(ll a, ll b) { return a + b; }
+    ll tree[4*MAX], lazy[4*MAX];
+    ll sz; // size of the array
+    int le(int n) { return 2*n+1; }
+    int ri(int n) { return 2*n+2; }
+    void push(int n, int esq, int dir) {
+        tree[n] += lazy[n] * (dir - esq + 1);
+        if (esq != dir) {
+            lazy[le(n)] += lazy[n];
+            lazy[ri(n)] += lazy[n];
+        }
+        lazy[n] = 0;
     }
-    lazy[n] = 0;
-}
-
-void build(int n, int esq, int dir, vi &v) {
-    if (esq == dir) tree[n] = v[esq];
-    else {
-        int mid = (esq + dir) / 2;
-        build(le(n), esq, mid, v);
-        build(ri(n), mid+1, dir, v);
-        tree[n] = tree[le(n)] + tree[ri(n)];
+    void build(span<const ll> v, int n, int esq, int dir) {
+        if (esq == dir) tree[n] = v[esq];
+        else {
+            int mid = (esq + dir) / 2;
+            build(v, le(n), esq, mid);
+            build(v, ri(n), mid+1, dir);
+            tree[n] = merge(tree[le(n)], tree[ri(n)]);
+        }
     }
-}
-void build(vi &v) {build(0, 0, n-1, v);}
-
-int query(int n, int esq, int dir, int l, int r) {
-    push(n, esq, dir);
-    if (esq > r || dir < l) return 0;
-    if (l <= esq && dir <= r) return tree[n];
-    int mid = (esq + dir) / 2;
-    return query(le(n), esq, mid, l, r)
-           + query(ri(n), mid+1, dir, l, r);
-}
-int query(int l, int r) {return query(0, 0, n-1, l, r);}
-
-void update(int n, int esq, int dir, int l, int r, int v) {
-    push(n, esq, dir);
-    if (esq > r || dir < l) return;
-    if (l <= esq && dir <= r) {
-        lazy[n] += v;
+    void build(span<const ll> v) { sz = v.size(); build(v, 0, 0, sz-1); }
+    ll query(int l, int r, int n=0, int esq=0, int dir=sz-1) {
         push(n, esq, dir);
-    } else {
-        int mid = (esq + dir) / 2;
-        update(le(n), esq, mid, l, r, v);
-        update(ri(n), mid+1, dir, l, r, v);
-        tree[n] = tree[le(n)] + tree[ri(n)];
+        if (esq > r || dir < l) return NEUTRAL;
+        if (l <= esq && dir <= r) return tree[n];
+        int mid = (esq + dir)/2;
+        return merge(query(l, r, le(n), esq, mid), query(l, r, ri(n), mid+1, dir));
     }
-}
-void update(int l, int r, int v) {update(0, 0, n-1, l, r, v);}
-
-int main() {
-    cin >> n;
+    void update(int l, int r, ll v, int n=0, int esq=0, int dir=sz-1) {
+        push(n, esq, dir);
+        if (esq > r || dir < l) return;
+        if (l <= esq && dir <= r) {
+            lazy[n] += v;
+            push(n, esq, dir);
+        } else {
+            int mid = (esq + dir)/2;
+            update(l, r, v, le(n), esq, mid);
+            update(l, r, v, ri(n), mid+1, dir);
+            tree[n] = merge(tree[le(n)], tree[ri(n)]);
+        }
+    }
 }
