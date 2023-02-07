5f4ec8819417d2ab6f85ef8580b51880060856d5
Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
2023-02-07 11:23:04 -0300

    Update HLD.cpp

--- Grafos/HLD/HLD.cpp
+++ Grafos/HLD/HLD.cpp
@@ -1,6 +1,8 @@
 namespace hld {
     const int MAX = 2e5+5;
     int t, sz[MAX], pos[MAX], pai[MAX], head[MAX];
+    bool e = 0;
+    ll merge(ll a, ll b) { return max(a, b); } // how to merge paths
     void dfs_sz(int u, int p=-1) {
         sz[u] = 1;
         for (int &v : adj[u]) if (v != p) {
@@ -24,18 +26,34 @@ namespace hld {
         head[root] = root;
         dfs_hld(root);
     }
+    void build(int root, vector<ll>& v) {
+        build(root);
+        vector<ll> aux(v.size());
+        for (int i = 0; i < (int)v.size(); i++) aux[pos[i]] = v[i];
+        seg::build(aux);
+    }
+    void build(int root, vector<i3>& edges) {
+        build(root);
+        e = 1;
+        vector<ll> aux(edges.size()+1);
+        for (auto [u, v, w]: edges) {
+            if (pos[u] > pos[v]) swap(u, v);
+            aux[pos[v]] = w;
+        } 
+        seg::build(aux);
+    }
     ll query(int u, int v) {
         if (pos[u] > pos[v]) swap(u, v);
-        if (head[u] == head[v]) return seg::query(pos[u], pos[v]); // pos[u]+1 for edges
+        if (head[u] == head[v]) return seg::query(pos[u]+e, pos[v]);
         else {
             ll qv = seg::query(pos[head[v]], pos[v]);
             ll qu = query(u, pai[head[v]]);
-            return max(qu, qv); // how to merge paths
+            return max(qu, qv);
         }
     }
     void update(int u, int v, ll k) {
         if (pos[u] > pos[v]) swap(u, v);
-        if (head[u] == head[v]) seg::update(pos[u], pos[v], k); // pos[u]+1 for edges
+        if (head[u] == head[v]) seg::update(pos[u]+e, pos[v], k);
         else {
             seg::update(pos[head[v]], pos[v], k);
             update(u, pai[head[v]], k);
