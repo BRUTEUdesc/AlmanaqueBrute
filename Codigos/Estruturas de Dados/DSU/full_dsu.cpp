a55cc118762d0b99ba046fecec4dfb0b95e09b94
João Oliveira <joaomarcosnet10@gmail.com>
2024-01-03 11:08:34 -0300

    full dsu

--- Codigos/Estruturas de Dados/DSU/full_dsu.cpp
+++ Codigos/Estruturas de Dados/DSU/full_dsu.cpp
@@ -47,35 +47,18 @@ struct full_dsu {
     void ord(int &a, int &b) {
         if (a > b) { swap(a, b); }
     }
+
     void add(int u, int v) {
         ord(u, v);
         edges[{u, v}].push_back({time++, (int)1e9, u, v, 0});
     }
+
     void remove(int u, int v) {
         ord(u, v);
         edges[{u, v}].back().r = time++;
     }
 
-    // consulta se os vértices estão no mesmo grupo
-    void question(int u, int v) {
-        ord(u, v);
-        queries.push_back({time, time, u, v, 1});
-        ++time;
-    }
-
-    void ord(int &a, int &b) {
-        if (a > b) { swap(a, b); }
-    }
-    void add(int u, int v) {
-        ord(u, v);
-        edges[{u, v}].push_back({time++, (int)1e9, u, v, 0});
-    }
-    void remove(int u, int v) {
-        ord(u, v);
-        edges[{u, v}].back().r = time++;
-    }
-
-    // consulta se dois vertices estao no mesmo grupo
+    // consulta se os vertices estao no mesmo grupo
     void question(int u, int v) {
         ord(u, v);
         queries.push_back({time, time, u, v, 1});
