637be11470070a6cec3384f8306bca85439ecc7d
Enzo <enzo200325@gmail.com>
2024-02-18 21:27:13 -0300

    Primos -> Miller-Rabin (sieve.cpp e naive.cpp foram de base)

---
+++ Codigos/Matemática/Miller-Rabin/README.md
@@ -0,0 +1,5 @@
+# [Miller-Rabin](miller_rabin.cpp)
+
+Teste de primalidade garantido para números menores do que $2^64$.
+
+- Complexidade de tempo: $\mathcal{O}(log(N))$
--- Codigos/Matemática/Primos/naive_is_prime.cpp
+++
@@ -1,8 +0,0 @@
-bool is_prime(int n) {
-    for (long long d = 2; d * d <= n; d++) {
-        if (n % d == 0) {
-            return false;
-        }
-    }
-    return true;
-}
