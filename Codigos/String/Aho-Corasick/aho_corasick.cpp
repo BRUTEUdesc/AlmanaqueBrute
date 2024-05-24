cde90e86f3d5fb37121505ee9f5501619d454310
João Oliveira <joaomarcosnet10@gmail.com>
2024-05-24 11:06:04 -0300

    binary lifting namespace, aho namespace

--- Codigos/String/Aho-Corasick/aho_corasick.cpp
+++ Codigos/String/Aho-Corasick/aho_corasick.cpp
@@ -1,34 +1,29 @@
-struct AC {
+namespace aho {
+    const int M = 3e5 + 1;
     const int K = 26;
+
     const char norm = 'a';
     inline int get(int c) { return c - norm; }
 
-    vector<int> link, out_link, par;
-    vector<char> pch;
-    vector<vector<int>> next;
-    vector<bool> out;
-    vector<vector<int>> output;
+    int next[M][K], link[M], out_link[M], par[M], cur = 2;
+    char pch[M];
+    bool out[M];
+    vector<int> output[M];
 
-    AC() { node(); }
-
-    int node(int p = -1, char c = -1) {
-        link.push_back(-1);
-        out_link.push_back(-1);
-        par.push_back(p);
-        pch.push_back(c);
-        next.push_back(vector<int>(K, -1));
-        out.push_back(false);
-        output.push_back(vector<int>());
-        return (int)link.size() - 1;
+    int node(int p, char c) {
+        link[cur] = out_link[cur] = 0;
+        par[cur] = p;
+        pch[cur] = c;
+        return cur++;
     }
 
     int T = 0;
 
     int insert(const string &s) {
-        int u = 0;
+        int u = 1;
         for (int i = 0; i < (int)s.size(); i++) {
             auto v = next[u][get(s[i])];
-            if (v == -1) {
+            if (v == 0) {
                 next[u][get(s[i])] = v = node(u, s[i]);
             }
             u = v;
@@ -38,28 +33,30 @@ struct AC {
         return T++;
     }
 
+    int go(int u, char c);
+
     int get_link(int u) {
-        if (link[u] == -1) {
-            link[u] = par[u] > 0 ? go(get_link(par[u]), pch[u]) : 0;
+        if (link[u] == 0) {
+            link[u] = par[u] > 1 ? go(get_link(par[u]), pch[u]) : 1;
         }
         return link[u];
     }
 
     int go(int u, char c) {
-        if (next[u][get(c)] == -1) {
-            next[u][get(c)] = u ? go(get_link(u), c) : 0;
+        if (next[u][get(c)] == 0) {
+            next[u][get(c)] = u > 1 ? go(get_link(u), c) : 1;
         }
         return next[u][get(c)];
     }
 
     int exit(int u) {
-        if (out_link[u] == -1) {
+        if (out_link[u] == 0) {
             int v = get_link(u);
-            out_link[u] = (out[v] || !v) ? v : exit(v);
+            out_link[u] = (out[v] || v == 1) ? v : exit(v);
         }
         return out_link[u];
     }
 
-    bool matched(int u) { return out[u] || exit(u) != 0; }
+    bool matched(int u) { return out[u] || exit(u) > 1; }
 
-} aho;
+}
