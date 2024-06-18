802c6d58f643154b37133a46793133a802b07d7e
João Oliveira <joaomarcosnet10@gmail.com>
2024-06-18 14:04:22 -0300

    better pollard rho

--- Codigos/Matemática/Fatoração-e-Primos/Pollard-Rho/pollard_rho.cpp
+++ Codigos/Matemática/Fatoração-e-Primos/Pollard-Rho/pollard_rho.cpp
@@ -1,13 +1,26 @@
 namespace PollardRho {
-    ll mod_mul(ll a, ll b, ll m) { return (ll)((__int128)a * b % m); }
+    mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+    const ll P = 5e6 + 1;
+    ll seq[P];
+    inline ll add_mod(ll x, ll y, ll m) { return (x += y) < m ? x : x - m; }
+    inline ll mul_mod(ll a, ll b, ll m) { return (ll)((__int128)a * b % m); }
     ll rho(ll n) {
-        auto f = [n](ll x) { return mod_mul(x, x, n) + 1; };
-        ll x = 0, y = 0, t = 30, prd = 2, i = 1, q;
-        while (t++ % 40 || gcd(prd, n) == 1) {
-            if (x == y) x = ++i, y = f(x);
-            if ((q = mod_mul(prd, max(x, y) - min(x, y), n))) prd = q;
-            x = f(x), y = f(f(y));
+        while (1) {
+            ll x = rng() % n, y = x, c = rng() % n, u = 1, v, t = 0;
+            ll *px = seq, *py = seq;
+            while (1) {
+                *py++ = y = add_mod(mul_mod(y, y, n), c, n);
+                *py++ = y = add_mod(mul_mod(y, y, n), c, n);
+                if ((x = *px++) == y) break;
+                v = u;
+                u = mul_mod(u, abs(y - x), n);
+                if (!u) return gcd(v, n);
+                if (++t == 32) {
+                    t = 0;
+                    if ((u = gcd(u, n)) > 1 && u < n) return u;
+                }
+            }
+            if (t && (u = gcd(u, n)) > 1 && u < n) return u;
         }
-        return gcd(prd, n);
     }
 }
