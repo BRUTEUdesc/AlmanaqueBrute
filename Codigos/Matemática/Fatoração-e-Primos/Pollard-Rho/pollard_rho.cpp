64acff8ff3600680881b70a5d9870cfcdf185267
João Oliveira <joaomarcosnet10@gmail.com>
2024-06-19 03:38:13 -0300

    better pollard rho

--- Codigos/Matemática/Fatoração-e-Primos/Pollard-Rho/pollard_rho.cpp
+++ Codigos/Matemática/Fatoração-e-Primos/Pollard-Rho/pollard_rho.cpp
@@ -1,12 +1,15 @@
 namespace PollardRho {
     mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
-    const ll P = 5e6 + 1;
+    const ll P = 1e6 + 1;
     ll seq[P];
     inline ll add_mod(ll x, ll y, ll m) { return (x += y) < m ? x : x - m; }
     inline ll mul_mod(ll a, ll b, ll m) { return (ll)((__int128)a * b % m); }
     ll rho(ll n) {
+        if (n % 2 == 0) return 2;
+        if (n % 3 == 0) return 3;
+        ll x0 = rng() % n, c = rng() % n;
         while (1) {
-            ll x = rng() % n, y = x, c = rng() % n, u = 1, v, t = 0;
+            ll x = x0++, y = x, u = 1, v, t = 0;
             ll *px = seq, *py = seq;
             while (1) {
                 *py++ = y = add_mod(mul_mod(y, y, n), c, n);
