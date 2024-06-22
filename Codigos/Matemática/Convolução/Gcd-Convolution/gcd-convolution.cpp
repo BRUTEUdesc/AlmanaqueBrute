4c3413ade3805e431aec3f0cdd5c0f1d1805f570
Eduardo Moreira <emoreira@Eduardos-MacBook-Air.local>
2024-06-22 18:08:48 -0300

    format

--- Codigos/Matemática/Convolução/Gcd-Convolution/gcd-convolution.cpp
+++ Codigos/Matemática/Convolução/Gcd-Convolution/gcd-convolution.cpp
@@ -1,21 +1,19 @@
-template<int MOD> vector<int> gcd_convolution(vector<int> A, vector<int> B) {
-  int N = int(max(A.size(), B.size()));
-  A.resize(N + 1);
-  B.resize(N + 1);
-  vector<int> C(N + 1);
-  for (int i = 1; i <= N; i++) {
-    int a = 0;
-    int b = 0;
-    for (int j = i; j <= N; j += i) {
-      a = (a + A[j]) % MOD;
-      b = (b + B[j]) % MOD;
+template <int MOD>
+vector<int> gcd_convolution(vector<int> A, vector<int> B) {
+    int N = int(max(A.size(), B.size()));
+    A.resize(N + 1);
+    B.resize(N + 1);
+    vector<int> C(N + 1);
+    for (int i = 1; i <= N; i++) {
+        int a = 0;
+        int b = 0;
+        for (int j = i; j <= N; j += i) {
+            a = (a + A[j]) % MOD;
+            b = (b + B[j]) % MOD;
+        }
+        C[i] = 1LL * a * b % MOD;
     }
-    C[i] = 1LL * a * b % MOD;
-  }
-  for (int i = N; i >= 1; i--) {
-    for (int j = 2 * i; j <= N; j += i) {
-      C[i] = (C[i] - C[j] + MOD) % MOD;
-    }
-  }
-  return C;
+    for (int i = N; i >= 1; i--)
+        for (int j = 2 * i; j <= N; j += i) C[i] = (C[i] - C[j] + MOD) % MOD;
+    return C;
 }
--- Codigos/Matemática/Convolução/Lcm-Convolution/lcm-convolution.cpp
+++ Codigos/Matemática/Convolução/Lcm-Convolution/lcm-convolution.cpp
@@ -1,19 +1,17 @@
-template<int MOD> vector<int> lcm_convolution(vector<int> A, vector<int> B) {
-  int N = int(max(A.size(), B.size()));
-  A.resize(N + 1);
-  B.resize(N + 1);
-  vector<int> C(N + 1), a(N + 1), b(N + 1);
-  for (int i = 1; i <= N; i++) {
-    for (int j = i; j <= N; j += i) {
-      a[j] = (a[j] + A[i]) % MOD;
-      b[j] = (b[j] + B[i]) % MOD;
+template <int MOD>
+vector<int> lcm_convolution(vector<int> A, vector<int> B) {
+    int N = int(max(A.size(), B.size()));
+    A.resize(N + 1);
+    B.resize(N + 1);
+    vector<int> C(N + 1), a(N + 1), b(N + 1);
+    for (int i = 1; i <= N; i++) {
+        for (int j = i; j <= N; j += i) {
+            a[j] = (a[j] + A[i]) % MOD;
+            b[j] = (b[j] + B[i]) % MOD;
+        }
+        C[i] = 1LL * a[i] * b[i] % MOD;
     }
-    C[i] = 1LL * a[i] * b[i] % MOD;
-  }
-  for (int i = 1; i <= N; i++) {
-    for (int j = 2 * i; j <= N; j += i) {
-      C[j] = (C[j] - C[i] + MOD) % MOD;
-    }
-  }
-  return C;
+    for (int i = 1; i <= N; i++)
+        for (int j = 2 * i; j <= N; j += i) C[j] = (C[j] - C[i] + MOD) % MOD;
+    return C;
 }
