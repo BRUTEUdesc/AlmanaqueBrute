407017e4c8aec373bde1704c9a45f8ec42949485
Eduardo Moreira <emoreira@Eduardos-MacBook-Air.local>
2024-06-22 18:07:49 -0300

    codigo e readme do or convolution

---
+++ Codigos/Matemática/Convolução/Or-Convolution/or-convolution.cpp
@@ -0,0 +1,30 @@
+template<int MOD> vector<int> or_convolution(vector<int> A, vector<int> B) {
+  int n = int(max(A.size(), B.size()));
+  int N = 0, two = 1;
+  while (two < n) {
+    two <<= 1;
+    N++;
+  }
+  A.resize(1 << N);
+  B.resize(1 << N);
+  vector<int> C(1 << N);
+  for (int j = 0; j < N; j++) {
+    for (int i = 0; i < 1 << N; i++) {
+      if (i >> j & 1) {
+        A[i] = (A[i] + A[i ^ (1 << j)]) % MOD;
+        B[i] = (B[i] + B[i ^ (1 << j)]) % MOD;
+      }
+    }
+  }
+  for (int i = 0; i < 1 << N; i++) {
+    C[i] = 1LL * A[i] * B[i] % MOD;
+  }
+  for (int j = N - 1; j >= 0; j--) {
+    for (int i = (1 << N) - 1; i >= 0; i--) {
+      if (i >> j & 1) {
+        C[i] = (C[i] - C[i ^ (1 << j)] + MOD) % MOD;
+      }
+    }
+  }
+  return C;
+}
