504ff05ef47441b381a0242a32be725e3071c437
João Oliveira <joaomarcosnet10@gmail.com>
2024-06-24 16:04:05 -0300

    mint operations

--- Codigos/Primitivas/Modular-Int/mint.cpp
+++ Codigos/Primitivas/Modular-Int/mint.cpp
@@ -9,16 +9,21 @@ struct Mint {
     m pwr(m b, ll e) {
         m res = 1;
         while (e > 0) {
-            if (e & 1) res = res * b;
-            b = b * b, e >>= 1;
+            if (e & 1) res *= b;
+            b *= b, e >>= 1;
         }
         return res;
     }
-    friend m operator-(m a, m b) { return (a.v -= b.v) < 0 ? a.v + MOD : a.v; }
-    friend m operator+(m a, m b) { return a - (MOD - b.v); }
-    friend m operator*(m a, m b) { return (T)((__int128)a.v * b.v % MOD); }
+    m& operator+=(m o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    m& operator-=(m o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    m& operator*=(m o) { v = (T)((__int128)v * o.v % MOD); return *this; }
     // se quiser otimizar constante, ao inves de (__int128) use (ll) se o T for `int`
-    friend m operator/(m a, m b) { return a * b.pwr(b, MOD - 2); }
+    m& operator/=(m o) { return *this *= o.pwr(o, MOD - 2); }
+    m& operator^=(ll e) { return *this = pwr(*this, e); }
+    friend m operator-(m a, m b) { return a -= b; }
+    friend m operator+(m a, m b) { return a += b; }
+    friend m operator*(m a, m b) { return a *= b; }
+    friend m operator/(m a, m b) { return a /= b; }
     friend m operator^(m a, ll e) { return a.pwr(a, e); }
 };
 
