dbfd7bc181aa786c365b3321791727f7a508ee74
EMachad0 <elitonmachadod200@gmail.com>
2024-06-28 15:49:14 -0300

    add kosaraju

---
+++ Codigos/Grafos/Kosaraju/kosaraju.cpp
@@ -0,0 +1,47 @@
+namespace kosaraju {
+    const int N = 1e5 + 5;
+    int n, vis[N], root[N];
+    vector<int> adj[N], inv[N], gc[N], topo;
+    void add_edge(int u, int v) {
+        adj[u].emplace_back(v);
+        inv[v].emplace_back(u);
+    }
+    void toposort(int u) {
+        vis[u] = 1;
+        for (auto v : adj[u]) {
+            if (vis[v]) continue;
+            toposort(v);
+        }
+        topo.emplace_back(u);
+    }
+    void dfs(int u) {
+        vis[u] = 1;
+        for (auto v : inv[u]) {
+            if (vis[v]) continue;
+            root[v] = root[u];
+            dfs(v);
+        }
+    }
+    void solve(int n) {
+        fill(vis, vis + n, 0);
+        topo.clear();
+        for (int i = 0; i < n; i++)
+            if (!vis[i]) toposort(i);
+        fill(vis, vis + n, 0);
+        iota(root, root + n, 0);
+        for (int i = n - 1; i >= 0; i--)
+            if (!vis[topo[i]]) dfs(topo[i]);
+        set<pair<int, int>> sete;
+        for (int u = 0; u < n; u++) {
+            int ru = root[u];
+            for (int v : adj[u]) {
+                int rv = root[v];
+                if (!sete.count(ii(ru, rv))) {
+                    gc[ru].emplace_back(rv);
+                    sete.insert(ii(ru, rv));
+                }
+            }
+        }
+    }
+}
+
