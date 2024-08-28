2ae2d6bcfe76f3df75eb4747f86f66eae454bf0c
eemoreira <112673452+eemoreira@users.noreply.github.com>
2024-08-28 15:26:47 -0300

    xor convolution (#158)
    * codigo e readme do and convolution
    
    * codigo e readme do gcd convolution
    
    * codigo e readme do lcm convolution
    
    * codigo e readme do or convolution
    
    * format
    
    * tirei '-' no nome do readme
    
    * fix nos readmes
    
    * fix nos readmes 2
    
    * tentando arrumar bug no readme do and convolution
    
    * tentando arrumar bug no readme do and convolution parte 2
    
    * tentando arrumar bug no readme do and convolution com chat gpt
    
    * tentando arrumar bug no readme ultima tentativa
    
    * tentando arrumar bug no readme ultima tentativa
    
    * tentando arrumar bug no readme ultima tentativa
    
    * mudei readme
    
    * typo
    
    * tentando arrumar bug no readme ultima tentativa
    
    * Update Codigos/Matemática/Convolução/And-Convolution/README.md
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/And-Convolution/and-convolution.cpp
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Gcd-Convolution/README.md
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Lcm-Convolution/README.md
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Or-Convolution/README.md
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Or-Convolution/or-convolution.cpp
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Lcm-Convolution/lcm-convolution.cpp
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Gcd-Convolution/gcd-convolution.cpp
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    * fomat no nome dos arquivos
    
    * mint nos codigos
    
    * Update Codigos/Matemática/Convolução/And-Convolution/and_convolution.cpp
    
    Co-authored-by: Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
    
    * Update Codigos/Matemática/Convolução/Or-Convolution/or_convolution.cpp
    
    Co-authored-by: Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>
    
    * Rename README.md to README.md
    
    * Rename and_convolution.cpp to and_convolution.cpp
    
    * Rename README.md to README.md
    
    * Rename gcd_convolution.cpp to gcd_convolution.cpp
    
    * Rename README.md to README.md
    
    * Rename lcm_convolution.cpp to lcm_convolution.cpp
    
    * Rename README.md to README.md
    
    * Rename or_convolution.cpp to or_convolution.cpp
    
    * xor convolution
    
    * xor symbol
    
    * xor symbol
    
    * minor fix no codigo
    
    ---------
    
    Co-authored-by: Eduardo Moreira <emoreira@Eduardos-MacBook-Air.local>
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    Co-authored-by: Eliton Machado da Silva <30319164+EMachad0@users.noreply.github.com>

---
+++ Codigos/Matemática/Convolução/XOR-Convolution/xor_convolution.cpp
@@ -0,0 +1,42 @@
+vector<mint> xor_convolution(vector<mint> A, vector<mint> B) {
+    int n = int(A.size());
+    for (int rep = 0; rep < 2; rep++) {
+        for (int len = n >> 1; len; len >>= 1) {
+            for (int i = 0; i < n; i += len << 1) {
+                for (int j = 0; j < len; j++) {
+                    int id = i + j;
+                    mint x = A[id];
+                    mint y = A[id + len];
+                    A[id] = x + y;
+                    A[id + len] = x - y;
+                }
+            }
+        }
+        swap(A, B);
+    }
+    vector<mint> ans(n);
+    for (int i = 0; i < n; i++) ans[i] = A[i] * B[i];
+    for (int len = 1; len < n; len <<= 1) {
+        for (int i = 0; i < n; i += len << 1) {
+            for (int j = 0; j < len; j++) {
+                int id = i + j;
+                mint x = ans[id];
+                mint y = ans[id + len];
+                ans[id] = x + y;
+                ans[id + len] = x - y;
+            }
+        }
+    }
+    return ans;
+}
+
+vector<mint> xor_multiply(vector<mint> A, vector<mint> B) {
+    int N = 1;
+    int n = int(max(A.size(), B.size()));
+    while (N < n) N <<= 1;
+    A.resize(N);
+    B.resize(N);
+    auto ans = xor_convolution(A, B);
+    for (int i = 0; i < N; i++) ans[i] /= N;
+    return ans;
+}
