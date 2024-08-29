1bd01bfc10e98096abc117e77925052f5c30baa0
eemoreira <112673452+eemoreira@users.noreply.github.com>
2024-08-29 16:16:15 -0300

    codigo segbeats nova (#161)
    * codigo segbeats nova
    
    * Update Codigos/Estruturas-de-Dados/Segment-Tree/Segment-Tree-Beats/README.md
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>

--- Codigos/Estruturas-de-Dados/Segment-Tree/Segment-Tree-Beats/seg_tree_beats.cpp
+++ Codigos/Estruturas-de-Dados/Segment-Tree/Segment-Tree-Beats/seg_tree_beats.cpp
@@ -1,83 +1,151 @@
-const int INF = 1e9;
-
-struct Node {
-    int m1 = INF, m2 = INF, cont = 0, lazy = 0;
-    ll soma = 0;
-    void set(int v) {
-        m1 = v;
-        cont = 1;
-        soma = v;
+const ll INF = 1e18;
+struct node {
+    ll mi, smi, mx, smx, sum, lazy;
+    int fmi, fmx;
+    node() {
+        mi = smi = INF;
+        mx = smx = -INF;
+        fmi = 0, fmx = 0, sum = 0, lazy = 0;
     }
-    void merge(Node a, Node b) {
-        m1 = min(a.m1, b.m1);
-        m2 = INF;
-        if (a.m1 != b.m1) m2 = min(m2, max(a.m1, b.m1));
-        if (a.m2 != m1) m2 = min(m2, a.m2);
-        if (b.m2 != m1) m2 = min(m2, b.m2);
-        cont = (a.m1 == m1 ? a.cont : 0) + (b.m1 == m1 ? b.cont : 0);
-        soma = a.soma + b.soma;
+    node(ll val) {
+        mi = mx = sum = val;
+        smi = INF, smx = -INF;
+        fmx = fmi = 1;
+        lazy = 0;
     }
 };
 
-struct SegTreeBeats {
-    int n, q;
-    vector<Node> t;
-    inline int lc(int n) { return 2 * n + 1; }
-    inline int rc(int n) { return 2 * n + 2; }
-    void push(int n, int esq, int dir) {
-        if (t[n].lazy <= t[n].m1) return;
-        t[n].soma += (ll)abs(t[n].m1 - t[n].lazy) * t[n].cont;
-        t[n].m1 = t[n].lazy;
-        if (esq != dir) {
-            t[lc(n)].lazy = max(t[lc(n)].lazy, t[n].lazy);
-            t[rc(n)].lazy = max(t[rc(n)].lazy, t[n].lazy);
-        }
-        t[n].lazy = 0;
+node operator+(node a, node b) {
+    node ret;
+    ret.sum = a.sum + b.sum;
+    if (a.mi == b.mi) {
+        ret.mi = a.mi;
+        ret.fmi = a.fmi + b.fmi;
+        ret.smi = min(a.smi, b.smi);
+    } else if (a.mi < b.mi) {
+        ret.mi = a.mi;
+        ret.fmi = a.fmi;
+        ret.smi = min(a.smi, b.mi);
+    } else {
+        ret.mi = b.mi;
+        ret.fmi = b.fmi;
+        ret.smi = min(b.smi, a.mi);
     }
-    void build(int n, int esq, int dir, const vector<ll> &v) {
-        if (esq == dir) {
-            t[n].set(v[esq]);
-        } else {
-            int mid = (esq + dir) / 2;
-            build(lc(n), esq, mid, v);
-            build(rc(n), mid + 1, dir, v);
-            t[n].merge(t[lc(n)], t[rc(n)]);
-        }
+    if (a.mx == b.mx) {
+        ret.mx = a.mx;
+        ret.fmx = a.fmx + b.fmx;
+        ret.smx = max(a.smx, b.smx);
+    } else if (a.mx > b.mx) {
+        ret.mx = a.mx;
+        ret.fmx = a.fmx;
+        ret.smx = max(b.mx, a.smx);
+    } else {
+        ret.fmx = b.fmx;
+        ret.mx = b.mx;
+        ret.smx = max(a.mx, b.smx);
     }
+    return ret;
+}
+
+struct SegBeats {
+    vector<node> t;
+    int n;
     void build(int _n) { // pra construir com tamanho, mas vazia
         n = _n;
-        t.assign(n * 4, Node());
+        t.assign(n * 4, node());
     }
     void build(const vector<ll> &v) { // pra construir com vector
         n = (int)v.size();
-        t.assign(n * 4, Node());
-        build(0, 0, n - 1, v);
+        t.assign(n * 4, node());
+        build(1, 0, n - 1, v);
     }
     void build(ll *bg, ll *en) { // pra construir com array de C
         build(vector<ll>(bg, en));
     }
-    // ai = max(ai, mi) em [l, r]
-    void update(int n, int esq, int dir, int l, int r, int mi) {
-        push(n, esq, dir);
-        if (esq > r || dir < l || mi <= t[n].m1) return;
-        if (l <= esq && dir <= r && mi < t[n].m2) {
-            t[n].lazy = mi;
-            push(n, esq, dir);
-        } else {
-            int mid = (esq + dir) / 2;
-            update(lc(n), esq, mid, l, r, mi);
-            update(rc(n), mid + 1, dir, l, r, mi);
-            t[n].merge(t[lc(n)], t[rc(n)]);
+    inline int lc(int p) { return 2 * p; }
+    inline int rc(int p) { return 2 * p + 1; }
+    node build(int p, int l, int r, const vector<ll> &a) {
+        if (l == r) return t[p] = node(a[l]);
+        int mid = (l + r) >> 1;
+        return t[p] = build(lc(p), l, mid, a) + build(rc(p), mid + 1, r, a);
+    }
+    void pushsum(int p, int l, int r, ll x) {
+        t[p].sum += (r - l + 1) * x;
+        t[p].mi += x;
+        t[p].mx += x;
+        t[p].lazy += x;
+        if (t[p].smi != INF) t[p].smi += x;
+        if (t[p].smx != -INF) t[p].smx += x;
+    }
+    void pushmax(int p, ll x) {
+        if (x <= t[p].mi) return;
+        t[p].sum += t[p].fmi * (x - t[p].mi);
+        if (t[p].mx == t[p].mi) t[p].mx = x;
+        if (t[p].smx == t[p].mi) t[p].smx = x;
+        t[p].mi = x;
+    }
+    void pushmin(int p, ll x) {
+        if (x >= t[p].mx) return;
+        t[p].sum += t[p].fmx * (x - t[p].mx);
+        if (t[p].mi == t[p].mx) t[p].mi = x;
+        if (t[p].smi == t[p].mx) t[p].smi = x;
+        t[p].mx = x;
+    }
+    void pushdown(int p, int l, int r) {
+        if (l == r) return;
+        int mid = (l + r) >> 1;
+        pushsum(lc(p), l, mid, t[p].lazy);
+        pushsum(rc(p), mid + 1, r, t[p].lazy);
+        t[p].lazy = 0;
+
+        pushmax(lc(p), t[p].mi);
+        pushmax(rc(p), t[p].mi);
+
+        pushmin(lc(p), t[p].mx);
+        pushmin(rc(p), t[p].mx);
+    }
+    node updatemin(int p, int l, int r, int L, int R, ll x) {
+        if (l > R || r < L || x >= t[p].mx) return t[p];
+        if (l >= L && r <= R && x > t[p].smx) {
+            pushmin(p, x);
+            return t[p];
         }
+        pushdown(p, l, r);
+        int mid = (l + r) >> 1;
+        t[p] = updatemin(lc(p), l, mid, L, R, x) + updatemin(rc(p), mid + 1, r, L, R, x);
+        return t[p];
     }
-    void update(int l, int r, int mi) { update(0, 0, n - 1, l, r, mi); }
-    // soma de [l, r]
-    ll query(int n, int esq, int dir, int l, int r) {
-        push(n, esq, dir);
-        if (esq > r || dir < l) return 0;
-        if (l <= esq && dir <= r) return t[n].soma;
-        int mid = (esq + dir) / 2;
-        return query(lc(n), esq, mid, l, r) + query(rc(n), mid + 1, dir, l, r);
+    node updatemax(int p, int l, int r, int L, int R, ll x) {
+        if (l > R || r < L || x <= t[p].mi) return t[p];
+        if (l >= L && r <= R && x < t[p].smi) {
+            pushmax(p, x);
+            return t[p];
+        }
+        pushdown(p, l, r);
+        int mid = (l + r) >> 1;
+        t[p] = updatemax(lc(p), l, mid, L, R, x) + updatemax(rc(p), mid + 1, r, L, R, x);
+        return t[p];
     }
-    ll query(int l, int r) { return query(0, 0, n - 1, l, r); }
+    node updatesum(int p, int l, int r, int L, int R, ll x) {
+        if (l > R || r < L) return t[p];
+        if (l >= L && r <= R) {
+            pushsum(p, l, r, x);
+            return t[p];
+        }
+        pushdown(p, l, r);
+        int mid = (l + r) >> 1;
+        return t[p] = updatesum(lc(p), l, mid, L, R, x) +
+                      updatesum(rc(p), mid + 1, r, L, R, x);
+    }
+    node query(int p, int l, int r, int L, int R) {
+        if (l > R || r < L) return node();
+        if (l >= L && r <= R) return t[p];
+        pushdown(p, l, r);
+        int mid = (l + r) >> 1;
+        return query(lc(p), l, mid, L, R) + query(rc(p), mid + 1, r, L, R);
+    }
+    ll query(int l, int r) { return query(1, 0, n - 1, l, r).sum; }
+    void updatemax(int l, int r, ll x) { updatemax(1, 0, n - 1, l, r, x); }
+    void updatemin(int l, int r, ll x) { updatemin(1, 0, n - 1, l, r, x); }
+    void updatesum(int l, int r, ll x) { updatesum(1, 0, n - 1, l, r, x); }
 } seg;
