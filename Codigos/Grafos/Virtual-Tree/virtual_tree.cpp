96a7df4e9e757f1a4bcfedb57121f783c7728a85
Eric <44513513+Grochowicz@users.noreply.github.com>
2024-09-11 20:32:24 -0300

    virtual tree mais curta

--- Codigos/Grafos/Virtual-Tree/virtual_tree.cpp
+++ Codigos/Grafos/Virtual-Tree/virtual_tree.cpp
@@ -1,32 +1,19 @@
 const int N = 3e5 + 5;
-#warning nao esqueca de copiar o codigo de LCA
+#warning nao esquece de copiar o codigo de LCA
 vector<int> vir_tree[N];
 vector<int> vir_nodes;
 
 void build_virtual_tree(vector<int> S) {
-    int n = (int)S.size();
     sort(S.begin(), S.end(), [&](int i, int j) { return bl::tin[i] < bl::tin[j]; });
-    for (int i = 1; i < n; i++) S.emplace_back(bl::lca(S[i - 1], S[i]));
+    for (int i = 1; i < (int)S.size(); i++) S.emplace_back(bl::lca(S[i - 1], S[i]));
     sort(S.begin(), S.end(), [&](int i, int j) { return bl::tin[i] < bl::tin[j]; });
     S.erase(unique(S.begin(), S.end()), S.end());
     vir_nodes = S;
-    n = (int)S.size();
     for (auto u : S) vir_tree[u].clear();
-    vector<int> stk = {S[0]};
-    for (int i = 1; i < n; i++) {
-        int u = S[i];
-        while (!bl::ancestor(stk.back(), u)) {
-            int v = stk.back();
-            stk.pop_back();
-            int pai = stk.back();
-            vir_tree[pai].emplace_back(v);
-        }
+    vector<int> stk;
+    for (auto u : S) {
+        while (stk.size() && !bl::ancestor(stk.back(), u)) stk.pop_back();
+        if(stk.size()) vir_tree[stk.back()].emplace_back(u);
         stk.emplace_back(u);
-    }
-    while (int(stk.size()) >= 2) {
-        int u = stk.back();
-        stk.pop_back();
-        int pai = stk.back();
-        vir_tree[pai].emplace_back(u);
     }
 }
