62cbf36c554252ed01cc2af4d602c7a8e31a374f
João Oliveira <70975757+joaomarcosth9@users.noreply.github.com>
2025-03-18 14:38:13 -0300

    Atualizar o suffix_automaton.cpp

--- Codigos/String/Suffix-Automaton/suffix_automaton.cpp
+++ Codigos/String/Suffix-Automaton/suffix_automaton.cpp
@@ -60,7 +60,7 @@ struct SuffixAutomaton {
             len[i] = lnk[i] = cnt[i] = 0;
             adj[i].clear();
         }
-        id = 2;
+        last = 1, id = 2, distinct_substrings = 0;
         for (int i = 0; i < n; i++) insert(i, s[i]);
         for (int i = 2; i < id; i++) adj[lnk[i]].push_back(i);
         dfs(1);
