05ca0072bcf5d38423368491ec2e782397bc62dc
Enzo de Almeida Rodrigues <nzo@Enzos-MacBook-Air.local>
2025-08-26 19:56:32 -0300

    ntt so com vector<mint> como parametro e format denovo...

--- Codigos/Matemática/NTT/NTT/ntt.cpp
+++ Codigos/Matemática/NTT/NTT/ntt.cpp
@@ -1,12 +1,12 @@
-template <auto MOD, typename T = Mint<MOD>>
-void ntt(vector<T> &a, bool inv = 0) {
+const int MOD = mod;
+void ntt(vector<mint> &a, bool inv = 0) {
     int n = (int)a.size();
     auto b = a;
-    T g = 1;
+    mint g = 1;
     while ((g ^ (MOD / 2)) == 1) g += 1;
-    if (inv) g = T(1) / g;
+    if (inv) g = mint(1) / g;
     for (int step = n / 2; step; step /= 2) {
-        T w = g ^ (MOD / (n / step)), wn = 1;
+        mint w = g ^ (MOD / (n / step)), wn = 1;
         for (int i = 0; i < n / 2; i += step) {
             for (int j = 0; j < step; j++) {
                 auto u = a[2 * i + j], v = wn * a[2 * i + j + step];
@@ -18,20 +18,19 @@ void ntt(vector<T> &a, bool inv = 0) {
         swap(a, b);
     }
     if (inv) {
-        T invn = T(1) / n;
+        mint invn = mint(1) / n;
         for (int i = 0; i < n; i++) a[i] *= invn;
     }
 }
 
-template <auto MOD, typename T = Mint<MOD>>
-vector<T> multiply(vector<T> a, vector<T> b) {
+vector<mint> multiply(vector<mint> a, vector<mint> b) {
     int n = (int)a.size(), m = (int)b.size();
     int t = n + m - 1, sz = 1;
     while (sz < t) sz <<= 1;
     a.resize(sz), b.resize(sz);
-    ntt<MOD>(a, 0), ntt<MOD>(b, 0);
+    ntt(a, 0), ntt(b, 0);
     for (int i = 0; i < sz; i++) a[i] *= b[i];
-    ntt<MOD>(a, 1);
+    ntt(a, 1);
     while ((int)a.size() > t) a.pop_back();
     return a;
 }
