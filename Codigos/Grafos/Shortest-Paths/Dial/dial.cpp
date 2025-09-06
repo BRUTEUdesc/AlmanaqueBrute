ec04ca616968b464e91e311bc0ec2d8febcf1d10
Eric <ericgrochowicz@hotmail.com>
2025-09-06 14:46:29 -0300

    padroniza nots e N

--- Codigos/Grafos/Shortest-Paths/Dial/dial.cpp
+++ Codigos/Grafos/Shortest-Paths/Dial/dial.cpp
@@ -1,4 +1,4 @@
-const int N = 1e5 + 5;
+const int N = 3e5 + 5;
 const int INF = 1e9;
 const int D = 61;
 
@@ -16,7 +16,7 @@ vector<int> dial(int s) {
         int x = -1;
         for(int i = 1; i <= D; i++) {
             int cur = (prev + i) % D;
-            if(not q[cur].empty()) {
+            if(!q[cur].empty()) {
                 x = cur;
                 break;
             }
@@ -24,7 +24,7 @@ vector<int> dial(int s) {
         if(x == -1) break;
         prev = x;
 
-        while(not q[x].empty()) {
+        while(!q[x].empty()) {
             int u = q[x].back();
             q[x].pop_back();
             if(x != dist[u] % D) continue;
